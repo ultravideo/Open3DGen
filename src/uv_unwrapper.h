6e092b34aba4b4cb3ef5cd78df8f3f23694da186
Teo Niemirepo <teo825@gmail.com>
2021-11-01 10:48:22 +0200

    Update uv_unwrapper.h

--- src/uv_unwrapper.h
+++ src/uv_unwrapper.h
@@ -19,12 +19,6 @@ namespace stitcher3d
 namespace uv
 {
   
-template <typename T>
-inline T min_arg(const T &a, const T &b)
-{
-  return a < b ? a : b;
-}
-
 void unwrap_uvs_xatlas(std::shared_ptr<surface::SurfaceMesh> mesh);
 
 Eigen::Vector2d get_point_uv(const Eigen::Vector3d& p0, const Eigen::Vector3d& p1,
